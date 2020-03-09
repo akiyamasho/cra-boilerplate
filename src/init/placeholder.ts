94ea0575f008e482d7d38391cf817b5d3e2ca4ed
ショウ <cvirtucio@cogent.co.jp>
2020-03-09 19:19:08 +0900

    chore: added results UI with placeholder

---
+++ src/init/placeholder.ts
@@ -0,0 +1,28 @@
+export const placeholderResultList: {
+    path: string;
+    lastUpdatedDate: string;
+    content: string;
+    matches: string[];
+}[] = [
+    {
+        path: "My Drive/Proposals/2013/Mitsubishi Motors",
+        lastUpdatedDate: "2019/03/21",
+        content:
+            "ルーフ、屋根（英: roof ）のこと。RUFオートモービル…ドイツの自動車会社... RUFオートモービル（ルーフオートモービル）は、創業者アロイス・ルーフ（初代）によるポルシェを基に独自の車造りを行うドイツの自動車メーカーである。一般のチューニングメーカーとは異なり、独自性の高い車造りを行い、そして排気ガスや耐久性等の社会的責任を持つことからドイツ自動車工業会（Verband der Automobilindustrie ）に所属が認められている。",
+        matches: ["屋根"],
+    },
+    {
+        path: "My Drive/Automotive/Project/Daihatsu Model X",
+        lastUpdatedDate: "2013/11/30",
+        content:
+            "サンルーフ（英語:Sunroof ）とは、自動車の屋根に装備される開口部である。ガラスや透明な合成樹脂の場合は天窓と呼ばれ、通常、その中で開閉式のものを指す。 チルトアップ式&スライド式（前席）とスライド式サンルーフを備えるミニバンの例（日産・プレサージュ）左が閉じた状態、右が開いた状態。",
+        matches: ["屋根"],
+    },
+    {
+        path: "My Drive/Automotive/Product/Car Ceiling",
+        lastUpdatedDate: "2018/12/03",
+        content:
+            "外車の天井は接着剤が日本の気候に合わないようで新車から数年で剥がれてきてしまうことも少なくありません。 特にジャガーやフォルクスワーゲン、BMW、ポルシェなどの欧州車は天井が最もはがれやすくなります。一度剥がれてしまうと剥がして改めて張り付けない限り綺麗にはなりません。屋根...",
+        matches: ["屋根"],
+    },
+];
--- src/search-results/container.tsx
+++ src/search-results/container.tsx
@@ -10,6 +10,9 @@ import SearchEngineSelection, {
 } from "../init/ui/custom/SearchEngineSelection";
 import PoweredByCogent from "../init/ui/custom/PoweredByCogent";
 import SearchInput from "../init/ui/custom/SearchInput";
+import Result, { SMALL_LOGO_WIDTH_PX } from "./components/Result";
+
+import { placeholderResultList } from "../init/placeholder";
 
 const SEARCH_BAR_HEIGHT_PX = 125;
 
@@ -22,6 +25,7 @@ const TopBarWrapper = styled.div`
     position: fixed;
     border-bottom: 1px solid ${hintTextColour};
     width: calc(100vw - 20px);
+    background: #ffffff;
 `;
 
 const SearchWrapper = styled.div`
@@ -36,8 +40,8 @@ const ResultsWrapper = styled.div`
 `;
 
 const Logo = styled.img`
-    height: 65px;
-    width: auto;
+    height: auto;
+    width: ${SMALL_LOGO_WIDTH_PX}px;
 `;
 
 const SearchResults = ({ results }: { results: Object[] }) => {
@@ -64,7 +68,29 @@ const SearchResults = ({ results }: { results: Object[] }) => {
                 </MidAlignedWrapper>
                 <PoweredByCogent />
             </TopBarWrapper>
-            <ResultsWrapper>results</ResultsWrapper>
+            <ResultsWrapper>
+                {placeholderResultList.map(
+                    ({
+                        path,
+                        lastUpdatedDate,
+                        content,
+                        matches,
+                    }: {
+                        path: string;
+                        lastUpdatedDate: string;
+                        content: string;
+                        matches: string[];
+                    }) => (
+                        <Result
+                            path={path}
+                            lastUpdatedDate={lastUpdatedDate}
+                            content={content}
+                            matches={matches}
+                        />
+                    )
+                )}
+                }
+            </ResultsWrapper>
         </MainContainer>
     );
 };
