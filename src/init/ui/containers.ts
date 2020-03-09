8e820c9cbba4640cd4b8cf97acb0d23a926c0118
ショウ <cvirtucio@cogent.co.jp>
2020-03-09 14:43:54 +0900

    chore: added containers

---
+++ src/init/ui/containers.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const MidAlignedWrapper = styled.div`
+    display: flex;
+    align-items: center;
+`;
